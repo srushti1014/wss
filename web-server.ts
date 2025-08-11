5fd82e5da841caa137118bd59dad4b23bece07d5
srushti <srushtipatel4040@gmail.com>
2025-08-11 16:36:15 +0530

    changes

--- web-server.ts
+++ web-server.ts
@@ -1,69 +1,9 @@
-// import { WebSocketServer, WebSocket } from "ws";
-// import http from "http";
-
-// const PORT = process.env.PORT || 3001;
-
-// // Store mapping of socket -> spaceId
-// const clients = new Map<WebSocket, string>();
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { "Content-Type": "text/plain" });
-//   res.end("OK");
-// });
-// const wss = new WebSocketServer({ server });
-
-// wss.on("connection", (socket) => {
-//   console.log("New client connected");
-
-//   socket.on("message", (message) => {
-//     try {
-//       const parsed = JSON.parse(message.toString());
-//       console.log("Received:", parsed);
-
-//       if (parsed.type === "join-room") {
-//         const spaceId = parsed.data?.spaceId;
-//         if (spaceId) {
-//           clients.set(socket, spaceId);
-//           console.log(`Socket joined space ${spaceId}`);
-//         }
-//         return; // no need to broadcast join-room
-//       }
-
-//       // Broadcast only to clients in the same spaceId
-//       const spaceId = parsed.data?.spaceId;
-//       if (spaceId) {
-//         wss.clients.forEach((client) => {
-//           if (
-//             client.readyState === WebSocket.OPEN &&
-//             clients.get(client) === spaceId
-//           ) {
-//             client.send(JSON.stringify(parsed));
-//           }
-//         });
-//       }
-//     } catch (err) {
-//       console.error("Invalid message", err);
-//     }
-//   });
-
-//   socket.on("close", () => {
-//     console.log("Client disconnected");
-//     clients.delete(socket);
-//   });
-// });
-
-// server.listen(PORT, () => {
-//   console.log(`WebSocket Server running on ws://localhost:${PORT}`);
-// });
-
-
-
 import { WebSocketServer, WebSocket } from "ws";
 import http from "http";
 
 const PORT = process.env.PORT || 3001;
 
-// roomId -> Set of sockets
+// roomId for Set of sockets
 const rooms: Map<string, Set<WebSocket>> = new Map();
 
 const server = http.createServer((req, res) => {
