59b4325f05c874cc1d41e6d05d2fd7e97da87014
srushti <srushtipatel4040@gmail.com>
2025-08-11 10:16:06 +0530

    changes

--- web-server.ts
+++ web-server.ts
@@ -4,8 +4,13 @@ import http from "http";
 
 const PORT = process.env.PORT || 3001;
 
-const server = http.createServer();
+// const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("OK");
+});
 const wss = new WebSocketServer({ server });
+
 
 wss.on("connection", (socket) => {
   console.log("New client connected");
@@ -13,8 +18,7 @@ wss.on("connection", (socket) => {
   socket.on("message", (message) => {
     console.log("Received:", message.toString());
 
-    // Echo back
-    socket.send(`Server received: ${message}`);
+    socket.send(message.toString());
   });
 
   socket.on("close", () => {
